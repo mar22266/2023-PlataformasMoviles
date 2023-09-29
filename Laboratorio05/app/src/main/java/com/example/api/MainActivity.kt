ff7f1bf50bddbcfbcc468359a8517ecda3f0679c
Andre <andremt2003@gmail.co>
2023-09-28 19:01:14 -0600

    avances de metodos y vistas

--- Laboratorio05/app/src/main/java/com/example/api/MainActivity.kt
+++ Laboratorio05/app/src/main/java/com/example/api/MainActivity.kt
@@ -6,63 +6,30 @@ package com.example.api
  * 22266
  */
 
-import android.app.ProgressDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.bumptech.glide.Glide
-import com.example.api.databinding.ActivityMainBinding
-import org.json.JSONObject
-import kotlin.math.log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-
-    val url: String = "https://meme-api.com/gimme"
-    lateinit var binding: ActivityMainBinding
+    private lateinit var button: Button
+    private lateinit var button2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        getMeme()
-        binding.nextmeme.setOnClickListener {
-            getMeme()
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
         }
-
-    }
-
-    fun getMeme() {
-        val progres = ProgressDialog(this)
-        progres.setMessage("Loading...")
-        progres.show()
-
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            { response ->
-                Log.e("Response", "getMeme:" +response.toString())
-
-                var resoneObject=JSONObject(response)
-
-                resoneObject.get("postLink")
-                binding.memetitle.text=resoneObject.getString("title")
-                binding.memeauthor.text=resoneObject.getString("author")
-
-                Glide.with(this).load(resoneObject.get("url")).into(binding.memeimage)
-                progres.dismiss()
-            },
-            { error->
-                progres.dismiss()
-                Toast.makeText(this@MainActivity,"${error.localizedMessage}",Toast.LENGTH_SHORT)
-                .show()
-            })
-
-        queue.add(stringRequest)
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
